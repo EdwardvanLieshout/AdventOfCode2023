2fde88e600461b20715386bc8dd746afc21d4eb2
EdwardvanLieshout <edward@edward.games>
2023-12-01 20:07:04 +0100

    Day1

---
+++ challenge1.ts
@@ -0,0 +1,9 @@
+import { readFileSync } from "fs";
+
+const file = readFileSync("./input1", "utf-8");
+const arr = file
+  .split(/\r?\n/)
+  .map((val) => val.split("").filter((char) => char.match(/\d/)))
+  .map((numvalues) => +(numvalues[0] + numvalues.pop()));
+
+console.log(arr.reduce((p, a) => p + a, 0));
