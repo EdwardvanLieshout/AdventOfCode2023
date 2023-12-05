bc9d8140e8b46163755e25442f5d80c4d8f56fde
EdwardvanLieshout <edward@edward.games>
2023-12-05 19:20:38 +0100

    Day5

---
+++ challenge5a.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from 'fs'
+
+const file = readFileSync('./input5', 'utf-8')
+let arr = file.split(/\r?\n/)
+let seeds = arr[0]
+    .substring(arr[0].indexOf(':') + 2)
+    .split(' ')
+    .map((seed) => +seed)
+arr.shift()
+let maps: any = file.split('map:')
+maps.shift()
+maps = maps.map((map) =>
+    map
+        .trim()
+        .split(/\r?\n/)
+        .filter((line) => line.match(/.+ .+ .+/))
+)
+maps.forEach((map) => {
+    let tempseeds = []
+    map.forEach((line) => {
+        const vals = line.split(' ')
+        seeds.forEach((seed, index) => {
+            if (seed >= +vals[1] && seed < +vals[1] + +vals[2]) {
+                tempseeds.push(+vals[0] + (seed - +vals[1]))
+                seeds[index] = -1
+            }
+        })
+        seeds = seeds.filter((seed) => seed !== -1)
+    })
+    seeds.push(...tempseeds)
+    console.log(seeds.sort((a, b) => a - b))
+})
---
+++ challenge5b.ts
@@ -0,0 +1,90 @@
+import { readFileSync } from 'fs'
+
+const file = readFileSync('./input5', 'utf-8')
+let arr = file.split(/\r?\n/)
+let seeds = arr[0]
+    .substring(arr[0].indexOf(':') + 2)
+    .split(' ')
+    .map((seed) => +seed)
+let seedRanges = []
+for (let i = 0; i < seeds.length; i += 2) {
+    seedRanges.push([seeds[i], seeds[i + 1]])
+}
+console.log(seedRanges)
+arr.shift()
+let maps: any = file.split('map:')
+maps.shift()
+maps = maps.map((map) =>
+    map
+        .trim()
+        .split(/\r?\n/)
+        .filter((line) => line.match(/.+ .+ .+/))
+)
+maps.forEach((map) => {
+    let tempseedRanges = []
+    map.forEach((line) => {
+        const vals = line.split(' ')
+        seedRanges.forEach((seedRange, index) => {
+            let adapted = false
+            if (seedRange[0] >= +vals[1] && seedRange[0] + seedRange[1] <= +vals[1] + +vals[2]) {
+                console.log('within', +vals[0], seedRange[0], +vals[1])
+                tempseedRanges.push([+vals[0] + seedRange[0] - +vals[1], seedRange[1]])
+                seedRanges[index] = [-1, -1]
+                adapted = true
+            }
+            if (
+                !adapted &&
+                seedRange[0] >= +vals[1] &&
+                seedRange[0] < +vals[1] + +vals[2] &&
+                seedRange[0] + seedRange[1] > +vals[1] + +vals[2] &&
+                seedRange[0] !== -1
+            ) {
+                console.log('right', +vals[0], seedRange[0], +vals[1], seedRange[1])
+                const start = seedRange[0]
+                const end = +vals[1] + +vals[2]
+                const final = seedRange[0] + seedRange[1]
+                tempseedRanges.push([+vals[0] + start - +vals[1], end - start])
+                seedRanges.push([end, final - end])
+                seedRanges[index] = [-1, -1]
+                adapted = true
+            }
+            if (
+                !adapted &&
+                seedRange[0] < +vals[1] &&
+                seedRange[0] + seedRange[1] > +vals[1] &&
+                seedRange[0] + seedRange[1] <= +vals[1] + +vals[2] &&
+                seedRange[0] !== -1
+            ) {
+                console.log('left', +vals[0], seedRange[0], +vals[1])
+                const start = +vals[1]
+                const end = seedRange[0] + seedRange[1]
+                const begin = seedRange[0]
+                tempseedRanges.push([+vals[0] + start - +vals[1], end - start])
+                seedRanges.push([begin, start - begin])
+                seedRanges[index] = [-1, -1]
+                adapted = true
+            }
+            if (
+                !adapted &&
+                seedRange[0] <= +vals[1] &&
+                seedRange[0] + seedRange[1] >= +vals[1] + +vals[2] &&
+                seedRange[0] !== -1
+            ) {
+                console.log('around', +vals[0], seedRange[0], +vals[1])
+                const start = +vals[1]
+                const end = +vals[1] + +vals[2]
+                const begin = seedRange[0]
+                const final = seedRange[0] + seedRange[1]
+                tempseedRanges.push([+vals[0] + start - +vals[1], end - start])
+                seedRanges.push([end, final - end])
+                seedRanges.push([begin, start - begin])
+                seedRanges[index] = [-1, -1]
+                adapted = true
+            }
+        })
+    })
+    seedRanges = seedRanges.filter((range) => range[0] >= 0)
+    seedRanges.push(...tempseedRanges)
+    console.log(seedRanges)
+    console.log(seedRanges.sort((a, b) => a[0] - b[0])[0][0])
+})
