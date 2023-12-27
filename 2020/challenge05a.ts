a82e4f898f75de3c2ecf7201d51adf98b46dca61
EdwardvanLieshout <edward@edward.games>
2023-12-27 22:57:45 +0100

    2020 Day5

--- 2020/challenge05a.ts
+++ 2020/challenge05a.ts
@@ -2,5 +2,10 @@ import { readFileSync } from 'fs';
 
 const file = readFileSync('./input05.input', 'utf-8');
 const input = file.split(/\r?\n/);
+const binary = input.map((input) =>
+    input.replaceAll('F', '0').replaceAll('B', '1').replaceAll('L', '0').replaceAll('R', '1')
+);
+const values = binary.map((str) => [parseInt(str.substring(0, 7), 2), parseInt(str.substring(7, 10), 2)]);
+const seatIDs = values.map((value) => value[0] * 8 + value[1]);
 
-console.log(input);
+console.log(seatIDs.sort((a, b) => b - a));
---
+++ 2020/challenge05b.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./input05.input', 'utf-8');
+const input = file.split(/\r?\n/);
+const binary = input.map((input) =>
+    input.replaceAll('F', '0').replaceAll('B', '1').replaceAll('L', '0').replaceAll('R', '1')
+);
+const values = binary.map((str) => [parseInt(str.substring(0, 7), 2), parseInt(str.substring(7, 10), 2)]);
+const seatIDs = values.map((value) => value[0] * 8 + value[1]);
+
+console.log(
+    seatIDs.find(
+        (id) =>
+            seatIDs.find((seat) => seat === id - 1) === undefined &&
+            seatIDs.find((seat) => seat === id - 2) !== undefined
+    ) - 1
+);
