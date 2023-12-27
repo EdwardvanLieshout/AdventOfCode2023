2dea90256dabc7c87189c704b16b6106b5bf5269
EdwardvanLieshout <edward@edward.games>
2023-12-27 20:56:05 +0100

    2020 Day3

--- 2020/challenge03a.ts
+++ 2020/challenge03a.ts
@@ -1,6 +1,23 @@
 import { readFileSync } from 'fs';
 
 const file = readFileSync('./input03.input', 'utf-8');
-const input = file.split(/\r?\n/);
+const map = file.split(/\r?\n/).map((line) => line.repeat(50).split(''));
 
-console.log(input);
+let ypos = 0;
+let xpos = 0;
+let treecount = 0;
+while (true) {
+    xpos += 3;
+    ypos++;
+    if (ypos >= map.length) {
+        break;
+    }
+    if (map[ypos][xpos] === undefined) {
+        throw new Error();
+    }
+    if (map[ypos][xpos] === '#') {
+        treecount++;
+    }
+}
+
+console.log(treecount);
---
+++ 2020/challenge03b.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./input03.input', 'utf-8');
+const map = file.split(/\r?\n/).map((line) => line.repeat(300).split(''));
+
+let ypos = 0;
+let xpos = 0;
+let treecount = 0;
+
+const checkTrees = (xi, yi): number => {
+    ypos = 0;
+    xpos = 0;
+    treecount = 0;
+    while (true) {
+        xpos += xi;
+        ypos += yi;
+        if (ypos >= map.length) {
+            break;
+        }
+        if (map[ypos][xpos] === undefined) {
+            throw new Error();
+        }
+        if (map[ypos][xpos] === '#') {
+            treecount++;
+        }
+    }
+    return treecount;
+};
+const val1 = checkTrees(1, 1);
+const val2 = checkTrees(3, 1);
+const val3 = checkTrees(5, 1);
+const val4 = checkTrees(7, 1);
+const val5 = checkTrees(1, 2);
+
+console.log(val1 * val2 * val3 * val4 * val5);
