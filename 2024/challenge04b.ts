3f1e1578d38565cd9dd9e82cb76a20efbd1df133
EdwardvanLieshout <edward@edward.games>
2024-12-04 06:37:52 +0100

    day4

---
+++ 2024/challenge04b.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./input04.input', 'utf-8');
+const input = file.split(/\r?\n/).map((row) => row.split(''));
+
+const isX_mas = (c1, c2, c3, c4, c5) =>
+    c1 === 'A' &&
+    ((c2 === 'M' && c5 === 'S') || (c2 === 'S' && c5 === 'M')) &&
+    ((c4 === 'M' && c3 === 'S') || (c4 === 'S' && c3 === 'M'));
+let count = 0;
+input.forEach((row, i) => {
+    row.forEach((char, j) => {
+        if (char === 'A') {
+            let bool1 = isX_mas(
+                input[i][j],
+                input[i - 1]?.[j - 1],
+                input[i - 1]?.[j + 1],
+                input[i + 1]?.[j - 1],
+                input[i + 1]?.[j + 1]
+            );
+            count += +bool1;
+        }
+    });
+});
+console.log(count);
