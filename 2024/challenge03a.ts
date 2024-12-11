c161687ec357d15162e7601de7c174bc3cbb7c3a
EdwardvanLieshout <edward@edward.games>
2024-12-11 19:53:48 +0100

    Turned day 3 part 1 into a oneliner

--- 2024/challenge03a.ts
+++ 2024/challenge03a.ts
@@ -1,9 +1,1 @@
-import { readFileSync } from 'fs';
-
-const file = readFileSync('./input03.input', 'utf-8');
-const input = file;
-let matches = [...input.matchAll(/mul\(\d+\,\d+\)/g)].map((match) => match[0]);
-let amtPairs = matches.map((match) => match.substring(4).slice(0, -1).split(','));
-let amts = amtPairs.map((amt) => +amt[0] * +amt[1]);
-
-console.log(amts.reduce((p, a) => p + a, 0));
+console.log([...(require('fs').readFileSync)('./input03.input','utf-8').matchAll(/mul\(\d+\,\d+\)/g)].map((v)=>v[0]).map((v)=>v.substring(4).slice(0,-1).split(',')).map((n)=>+n[0]*+n[1]).reduce((p,a)=>p+a,0));
